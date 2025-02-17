2128abe02779286f006fca4536f85d4125738d9f
Arnaud Levy <contact@arnaudlevy.com>
2025-02-17 11:10:42 +0100

    reports

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -7,5 +7,6 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     primary.item  :dashboard, t('admin.dashboard'), admin_root_path, { icon: 'bi bi-grid-fill', highlights_on: /admin$/ }
+    primary.item  :reports, Report.model_name.human(count: 2), admin_reports_path, { icon: 'bi bi-exclamation-circle-fill' }
   end
 end
