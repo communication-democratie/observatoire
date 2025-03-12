56ef2085828910af6a18cf8f1f4a888b0d0c98e6
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-03-12 11:39:45 +0100

    404 when taxonomy not found

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,25 @@
 class CategoriesController < ApplicationController
+  before_action :load_taxonomy
+
   def index
-    @taxonomy = Taxonomy.find_by(slug: params[:taxonomy_slug])
     @categories = @taxonomy.categories.ordered
     breadcrumb
   end
 
   def show
-    @taxonomy = Taxonomy.find_by(slug: params[:taxonomy_slug])
-    @category = Category.find_by(slug: params[:category_slug])
+    @category = Category.find_by!(slug: params[:category_slug])
     breadcrumb
   end
-  
+
   protected
+
+  def load_taxonomy
+    @taxonomy = Taxonomy.find_by!(slug: params[:taxonomy_slug])
+  end
 
   def breadcrumb
     super
-    add_breadcrumb @taxonomy, taxonomy_path(taxonomy_slug: @taxonomy.slug) if @taxonomy
+    add_breadcrumb @taxonomy, taxonomy_path(taxonomy_slug: @taxonomy.slug)
     add_breadcrumb @category, category_path(taxonomy_slug: @taxonomy.slug, category_slug: @category.slug) if @category
   end
 end
