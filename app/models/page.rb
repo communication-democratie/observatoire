962e76b619a2399f0fc136073fc44aec5df82b3b
Arnaud Levy <contact@arnaudlevy.com>
2025-04-08 08:47:52 +0200

    toc

--- app/models/page.rb
+++ app/models/page.rb
@@ -15,20 +15,18 @@ class Page < ApplicationRecord
   scope :ordered, -> { order(:position) }
 
   def html
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, with_toc_data: true)
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions = {})
+    redcarpet.render(markdown).html_safe
+  end
+
+  def toc
+    renderer = Redcarpet::Render::HTML_TOC
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions = {})
     redcarpet.render(markdown).html_safe
   end
 
   def to_s
     "#{title}"
-  end
-
-  protected
-
-  def renderer
-    @renderer ||= Redcarpet::Render::HTML.new(hard_wrap: true)
-  end
-
-  def redcarpet
-    @redcarpet ||= Redcarpet::Markdown.new(renderer, extensions = {})
   end
 end
