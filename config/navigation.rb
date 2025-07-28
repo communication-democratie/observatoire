64e6384153cbe982528c3d857e5f87476b73b706
Arnaud Levy <contact@arnaudlevy.com>
2025-07-28 17:25:32 +0200

    fix

--- config/navigation.rb
+++ config/navigation.rb
@@ -8,7 +8,6 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item  :analyzed_problems, 'Nos analyses', analyzed_problems_path
     Page.ordered.each do |page|
       primary.item page.slug.to_sym, page.title, "/#{page.slug}" do |secondary|
-        secondary.item page.slug.to_sym, page.title, "/#{page.slug}"
         page.h2_titles.each do |title, anchor|
           secondary.item anchor.to_sym, title, "/#{page.slug}#{anchor}"        
         end
