aa2e6423d04406a7f2f7267f73cbe53335dd7291
Arnaud Levy <contact@arnaudlevy.com>
2025-07-28 16:36:18 +0200

    menus

--- config/navigation.rb
+++ config/navigation.rb
@@ -6,10 +6,9 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item  :problems, 'Pubs signalées', problems_path
     primary.item  :analyzed_problems, 'Nos analyses', analyzed_problems_path
-    primary.item  :more, 'En savoir plus' do |secondary|
-      Page.ordered.each do |page|
-        secondary.item page.slug.to_sym, page.title, "/#{page.slug}"
-      end
+    Page.ordered.each do |page|
+      primary.item page.slug.to_sym, page.title, "/#{page.slug}"
     end
+    primary.item  :button, 'Signaler', '/#reporting', html: { class: 'header-cta' }
   end
 end
