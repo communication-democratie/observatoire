a17724f347f5e77936b20f720c0f51a2de6fd1db
pabois <pierreandre.boissinot@noesya.coop>
2025-07-31 10:49:04 +0200

    remove pages without positions from menu

--- config/footer_navigation.rb
+++ config/footer_navigation.rb
@@ -5,7 +5,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     primary.item  :analyzed_problems, 'Nos analyses', analyzed_problems_path
-    Page.ordered.each do |page|
+    Page.with_position.ordered.each do |page|
       primary.item page.slug.to_sym, page.title, "/#{page.slug}" do |secondary|
         page.h2_titles.each do |title, anchor|
           secondary.item anchor.to_sym, title, "/#{page.slug}#{anchor}"        
