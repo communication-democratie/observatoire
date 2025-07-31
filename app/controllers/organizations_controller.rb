79021684a3944596654e6ee1035601b12ece57b1
Arnaud Levy <contact@arnaudlevy.com>
2025-07-31 17:42:22 +0200

    front (waiting for org problems)

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,6 +2,7 @@ class OrganizationsController < ApplicationController
 
   def show
     @organization = Organization.find_by(slug: params.expect(:slug))
+    @problems = @organization.problems
     breadcrumb
     add_breadcrumb @organization
   end
