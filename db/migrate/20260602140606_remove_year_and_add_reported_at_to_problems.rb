b3fd8dd85315c54c4798e775012cf1589525af35
Arnaud Levy <contact@arnaudlevy.com>
2026-06-02 17:03:00 +0200

    Update db/migrate/20260602140606_remove_year_and_add_reported_at_to_problems.rb
    Co-authored-by: Sébastien Gaya <sebastien.gaya@gmail.com>

--- db/migrate/20260602140606_remove_year_and_add_reported_at_to_problems.rb
+++ db/migrate/20260602140606_remove_year_and_add_reported_at_to_problems.rb
@@ -2,5 +2,10 @@ class RemoveYearAndAddReportedAtToProblems < ActiveRecord::Migration[8.1]
   def change
     remove_column :problems, :year
     add_column :problems, :reported_at, :datetime
+    Problem.reset_column_information
+    Problem.all.each do |problem|
+      reported_at = problem.reports.ordered.last&.created_at || problem.created_at
+      problem.update_column :reported_at, reported_at
+    end
   end
 end
